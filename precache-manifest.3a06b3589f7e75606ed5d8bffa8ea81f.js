434e5cb962649fe2e56aa52d75b70c915d295723
dhushandhansd <dhushandhan.sd@gmail.com>
2020-11-25 20:35:20 +0530

    Updates

---
+++ precache-manifest.3a06b3589f7e75606ed5d8bffa8ea81f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff85f5c86c45519161f398b8f4054b7c",
+    "url": "/ukgrafix/index.html"
+  },
+  {
+    "revision": "1e0af325ca1c0a98441c",
+    "url": "/ukgrafix/static/css/main.1a88c260.chunk.css"
+  },
+  {
+    "revision": "aff958841254d7b3c03e",
+    "url": "/ukgrafix/static/js/2.d9429572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ukgrafix/static/js/2.d9429572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0af325ca1c0a98441c",
+    "url": "/ukgrafix/static/js/main.7b6a03cd.chunk.js"
+  },
+  {
+    "revision": "2a067a0a2387ede3a8ff",
+    "url": "/ukgrafix/static/js/runtime-main.22eec8ef.js"
+  },
+  {
+    "revision": "fe0356678e11aeb74ea7774940ae0a07",
+    "url": "/ukgrafix/static/media/balajee.fe035667.png"
+  },
+  {
+    "revision": "65bbc5c174ec1d633b9aa3b213d60331",
+    "url": "/ukgrafix/static/media/contactusimg.65bbc5c1.png"
+  },
+  {
+    "revision": "ecc0c7dcf2a468ffaa1769de2b1642b8",
+    "url": "/ukgrafix/static/media/developerimg1.ecc0c7dc.png"
+  },
+  {
+    "revision": "7f265f4ccf4006fac579866fe36f29e0",
+    "url": "/ukgrafix/static/media/homepagebgimg.7f265f4c.png"
+  },
+  {
+    "revision": "b1ad2ee694945a5d91506b07621028bd",
+    "url": "/ukgrafix/static/media/portfoliobgimg.b1ad2ee6.png"
+  },
+  {
+    "revision": "08181e0aaeaa34c8cd638447f19517bb",
+    "url": "/ukgrafix/static/media/projecctweb2.08181e0a.png"
+  },
+  {
+    "revision": "3055044bd6f2b3b85258a5fe67e55a62",
+    "url": "/ukgrafix/static/media/projectmob1.3055044b.png"
+  },
+  {
+    "revision": "21293aa7f711372ef9f8d780d8c3685b",
+    "url": "/ukgrafix/static/media/projectmob2.21293aa7.png"
+  },
+  {
+    "revision": "8e81587c721585dd489eb039f3c48c5a",
+    "url": "/ukgrafix/static/media/projectmob3.8e81587c.png"
+  },
+  {
+    "revision": "a82d223494a2894c5648f5089faa48f5",
+    "url": "/ukgrafix/static/media/projectmob6.a82d2234.png"
+  },
+  {
+    "revision": "24472efef373c5fd03eab3e924c24312",
+    "url": "/ukgrafix/static/media/projectmob7.24472efe.png"
+  },
+  {
+    "revision": "6f0d27937b4621914365ebac558a7758",
+    "url": "/ukgrafix/static/media/projectweb1.6f0d2793.png"
+  },
+  {
+    "revision": "67b147d783839ca49b83cce203c8f166",
+    "url": "/ukgrafix/static/media/projectweb3.67b147d7.png"
+  },
+  {
+    "revision": "8e0c0426d7f3d7a79198c2b59763527c",
+    "url": "/ukgrafix/static/media/projectweb4.8e0c0426.png"
+  },
+  {
+    "revision": "0f0c02b2318cdab9d101d994fc7f360d",
+    "url": "/ukgrafix/static/media/sabari.0f0c02b2.png"
+  },
+  {
+    "revision": "58b9be22f7d3ef22c8dfb98bfc7d70c2",
+    "url": "/ukgrafix/static/media/shree.58b9be22.png"
+  }
+]);
